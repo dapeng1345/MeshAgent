62709abf12911f90f37675f771f7884f5e5dfe7e
dapeng1345 <dapeng1345@gmail.com>
2026-09-06 11:35:13 +0800

    Add Windows watchdog declaration

--- meshcore/KVM/Windows/kvm.h
+++ meshcore/KVM/Windows/kvm.h
@@ -24,6 +24,7 @@ void kvm_pause(int pause);
 int kvm_relay_setup(char* exePath, void *processPipeMgr, ILibKVM_WriteHandler writeHandler, void *reserved, int tsid);
 void kvm_cleanup();
 void kvm_setupSasPermissions();
+void kvm_start_idle_watchdog();
 void kvm_relay_reset(ILibKVM_WriteHandler writeHandler, void *reserved);
 void kvm_server_SetResolution(ILibKVM_WriteHandler writeHandler, void *reserved);
 
